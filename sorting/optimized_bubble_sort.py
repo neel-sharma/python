74587a1dead946d00824a73f051e6717c39b3ef1
neel-sharma <75809664+neel-sharma@users.noreply.github.com>
2021-01-20 15:25:42 +0530

    Create optimized_bubble_sort.py

---
+++ sorting/optimized_bubble_sort.py
@@ -0,0 +1,32 @@
+# Code
+def bubble_sort(total,data):
+    for i in range(total-1):
+        swapped = False
+        for j in range(total - 1 - i):
+            if data[j] > data[j + 1]:
+                swapped = True
+                data[j], data[j + 1] = data[j + 1], data[j]
+        if not swapped:          #stop if sorted
+            break 
+    return data
+
+'''
+# Driver Code
+
+# Individually enter each value
+
+total=int(input('Total sum of numbers? '))
+data = []                      
+for i in range(0,total):       
+    data.append(int(input()))
+
+# Enter all values together
+
+#collective_data = input("Enter numbers separated by a comma:").strip()
+#data = [int(i) for i in collective_data.split(",")]
+#total=len(data)
+
+
+data=bubble_sort(total,data)
+print(data)
+'''
